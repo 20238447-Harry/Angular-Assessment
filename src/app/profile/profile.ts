173fbdee2fda9558df41c941b50f72815130003a
Harry Bahatan <20238447@s.ubaguio.edu>
2026-02-09 13:36:43 +0800

    Added Profile component with interface

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
 
-@Component({
+Component({
   selector: 'app-profile',
   imports: [],
   templateUrl: './profile.html',
   styleUrl: './profile.css',
 })
+interface User {
+  name: string;
+  email: string;
+  age: number;
+  location: string;
+  bio: string;
+}
+
+export class ProfileComponent {
+  user: User = {
+    name: 'Harry Jake Bahatan',
+    email: 'Bharryjake@email.com',
+    age: 20,
+    location: 'Philippines',
+    bio: 'IT Student who is learning about coding'
+  };
+}
+
 export class Profile {
 
 }
